33b6cf0d047661fb1e62ee7ac894be80bfc98ce8
Rhubarb <edlun066@d.umn.edu>
2019-03-06 04:40:35 -0600

    Movie Search Attempt 1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,49 +9,38 @@ $()
         "Magic Eye", // To see Magic Eye Illusions, unfocus your eyes and look "through" the screen.
     ];
 
-    var testURL = "http://api.giphy.com/v1/gifs/search?q=magic+eye&api_key=dc6zaTOxFJmzC"
-    //var queryURL = 
+    var objectURL = "http://api.giphy.com/v1/gifs/search?q=magic+eye&api_key=dc6zaTOxFJmzC";
+    
+    console.log("/1---------/");
 
     $.ajax({
-        url: testURL,
+        url: objectURL,
         method: "GET"
     }).done(function(response) {
         console.log(response);
     });
 
+    console.log("/2---------/");
+    
 
-    /*
-    $.ajax({
-        url: searchURL,
-        method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    });
+        console.log("This console.log will probably happen first because of asynchronicity.");
+        var x = 2;
+        var y = 10;
+        var z = 13;
+        console.log("We can also assign some variables and do some arithmetic while we wait too: 2 + 10 + 13 = ", x + y + z);
+    
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    });
-    */
+        //PROCESS
 
+        //When user clicks on one of the buttons, gifs show up of the topic
 
-    console.log("This console.log will probably happen first because of asynchronicity.");
-    var x = 2;
-    var y = 10;
-    var z = 13;
-    console.log("We can also assign some variables and do some arithmetic while we wait too: 2 + 10 + 13 = ", x + y + z);
-  
+        //I will need search bar
 
-    //FUNCTIONS
+        //I will need API key
 
-    //PROCESS
+        //I will have to connect the buttons to the gifs
 
-    //When user clicks on one of the buttons, gifs show up of the topic
+        //Limit: it will have to display 10 gifs per page
+        //Rating: the rating will be g or pg
 
-    //I will need search bar
-
-    //I will need API key
-
-    //I will have to connect the buttons to the gifs
+        console.log("/4---------/");
