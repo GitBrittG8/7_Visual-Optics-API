62bd7a57895ecfbff7769b8038d4032084a2bc22
Rhubarb <edlun066@d.umn.edu>
2019-03-09 00:00:29 -0600

    Search Responds with String... Add Button Deleted for debugging

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var topic = [
+    
+    var topics = [
         "Nature",
         "Assembly Line",
         "Chemical Reaction",
@@ -9,53 +10,20 @@ $(document).ready(function() {
         "Magic Eye", // To see Magic Eye Illusions, unfocus your eyes and look "through" the screen.
     ];
 
-    var objectURL = "http://api.giphy.com/v1/gifs/search?q=magic+eyeg&api_key=qOIsjLz9Eaf7IbS8C0UnGwJhPYZbrPrq&limit=10";
+    /// "FIND" BUTTON
+    
+    $("#find-giph").on("click", function(event) {
+        event.preventDefault();
+        var userInput = $("#user-input").val().trim();
+        var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=dc6zaTOxFJmzC&limit=5&rating=pg-13";
 
-    console.log("/1---------/");
-
-    $.ajax({
-        url: objectURL,
-        method: "GET"
-    }).done(function(response) {
-        console.log(response);
+        // Display User's search in giphy-view
+        $.ajax({
+            url: giphyURL,
+            method: "GET"
+        }).then(function(response) {
+            $("#giph-view").text(JSON.stringify(response));
+            console.log("User Input: " + userInput);
+        });
     });
-
-    console.log("/2---------/");
-    
-
-        console.log("This console.log will probably happen first because of asynchronicity.");
-        var x = 2;
-        var y = 10;
-        var z = 13;
-        console.log("We can also assign some variables and do some arithmetic while we wait too: 2 + 10 + 13 = ", x + y + z);
-    
-
-        //PROCESS
-
-        //When user clicks on one of the buttons, gifs show up of the topic
-
-        //I will need search bar
-
-        //I will need API key
-
-        //I will have to connect the buttons to the gifs
-
-        //Limit: it will have to display 10 gifs per page
-        //Rating: the rating will be g or pg
-        console.log("/3---------/");
-        $(document).on("click", "#find-giphy", function () { // Trigger AJAX Call
-            event.preventDefault(); //Prevents button from submitting default form          
-            var giphy = $("#giphy-input").val(); // Select text from input box
-            var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "g&api_key=qOIsjLz9Eaf7IbS8C0UnGwJhPYZbrPrq&limit=10"; 
-
-            $.ajax({
-                url: giphyURL,
-                method: "GET"
-
-            }).then(function(response) {
-                $("#giphy-view").text(JSON.stringify(response));
-                console.log("JSON response: " + response)
-            });
-        }); 
-        console.log("/4---------/");
 })
